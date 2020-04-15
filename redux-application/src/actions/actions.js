064e2d3f08aa2f36c6615bbc24542ca9bdeed30c
Jared Brown <jaredbrown1@hotmail.com>
2020-04-14 19:59:39 -0700

    actions created

---
+++ redux-application/src/actions/actions.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const FETCH_DATA = "FETCH_DATA";
+export const UPDATE_AMIIBO = "UPDATE_AMIIBO";
+export const SET_ERROR = "SET_ERROR";
+
+export const getData = () => (dispatch) => {
+  dispatch({ type: FETCH_DATA });
+  axios
+    .get("https://www.amiiboapi.com/api/amiibo/")
+    .then((res) => {
+      console.log("from axios promise", res);
+      dispatch({ type: UPDATE_AMIIBO, payload: { amiibo: res.data.amiibo } });
+    })
+    .catch((err) => {
+      console.log("error fetching data ", err);
+      dispatch({ type: SET_ERROR, payload: "error fetching data" });
+    });
+};
