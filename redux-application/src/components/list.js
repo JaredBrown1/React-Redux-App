02dce3bf7b69e336a9a48f2749510aec521e6286
Jared Brown <jaredbrown1@hotmail.com>
2020-04-16 17:52:08 -0700

    app styled

--- redux-application/src/components/list.js
+++ redux-application/src/components/list.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
 import { getData } from "../actions/actions";
+import styled from "styled-components";
+
+const Image = styled.img`
+  background-color: white;
+  border-radius: 50px;
+  height: 300px;
+`;
 
 const List = (props) => {
   return (
@@ -10,7 +17,7 @@ const List = (props) => {
       ) : (
         props.amiibo.map((amiibo) => (
           <div>
-            <img src={amiibo.image} />
+            <Image src={amiibo.image} />
             <h3>{amiibo.name}</h3>
           </div>
         ))
